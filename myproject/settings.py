2d934fea94f3b7d31023694078eb816665bae30b
dopraxcom <doprax.com@gmail.com>
2023-06-20 12:57:02 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q9^%73hzon-&z4@uq&%r^r9x77$p!^(e!@(u6w%k1(!s8^x+^e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.dopraxapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.dopraxrocks.com','127.0.0.1','localhost']
 
 
 # Application definition
